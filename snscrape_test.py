09999abb5f5c3fe990f7a6bd31927104fbaa6d27
William Cooper <cooperwilliam92@gmail.com>
2022-10-25 17:33:17 +0100

    Create loop to scrape accounts and add gitignore file

--- snscrape_test.py
+++ snscrape_test.py
@@ -1,20 +1,22 @@
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
 
-# Creating list to append tweet data to
-tweets_list = []
+# Create list of accounts to search
 accounts_list = ["CNBC", "FT", "Reuters"]
-keywords_list = ["Tesla", "Apple", "Amazon"]
 
-# Using TwitterSearchScraper to scrape data and append tweets to list
-# Currently adds all tweets from CNBC containing the keyword 'Tesla' that are not retweets or replies
-for i,tweet in enumerate(sntwitter.TwitterSearchScraper('Tesla since:2010-01-01 until:2022-12-31 from:CNBC').get_items()):
-    if i>20000:
-        break
-    if "@" not in tweet.content:
-        tweets_list.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
-    
-# Creating a dataframe from the tweets list above
-tweets_df = pd.DataFrame(tweets_list, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
-# Saving the dataframe in a CSV file
-tweets_df.to_csv('CNBC_tesla_tweets.csv')
+# Iterate across accounts listed
+for account in accounts_list:
+    # Create empty list to store scraped tweets
+    tweets_list = []
+    # Search for tweets since 2010 from the specified account whose text contains the keyword 'Tesla'
+    for i,tweet in enumerate(sntwitter.TwitterSearchScraper('Tesla since:2010-01-01 until:2022-12-31 from:' + account).get_items()):
+        if i>20000:
+            break
+        # Disallow replies and retweets
+        if "@" and "RT" not in tweet.content:
+            # Add relevant data to list
+            tweets_list.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
+        # Create a dataframe from the tweets_list above
+        tweets_df = pd.DataFrame(tweets_list, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
+        # Save dataframe in a CSV file with custom name according to account scraped
+        tweets_df.to_csv(account + '_tesla_tweets.csv')
