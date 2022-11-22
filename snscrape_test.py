bc002e4a7c39e9dfcd448517751c8238bec4d92d
William Cooper <cooperwilliam92@gmail.com>
2022-11-22 21:40:10 +0000

    Remove unnecessary comments

--- snscrape_test.py
+++ snscrape_test.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 # Create list of accounts to search
 accounts_list = ["CNBC","FT", "Reuters"]
-stocks_list = ["Tesla", "Amazon"]
+#stocks_list = ["Tesla", "Amazon"]
+stocks_list = ["Apple", "Netflix"]
 
 #Iterate across accounts listed
 for stock in stocks_list:
@@ -15,29 +16,10 @@ for stock in stocks_list:
             if i>20000:
                 break
             # Disallow replies and retweets
-            #print("REACHED BEFORE @ IF STATEMENT")
             if "@" and "RT" not in tweet.content:
                 # Add relevant data to list
-                #print("REACHED APPEND")
                 tweets_list.append([tweet.user.username, tweet.date, tweet.content])
     # Create dataframe from the tweets_list above
     tweets_df = pd.DataFrame(tweets_list, columns=['Username', 'Datetime', 'Text'])
     # Convert dataframe to CSV file with custom name
     tweets_df.to_csv(stock +'_all_tweets.csv')
-
-# for account in accounts_list:
-#     for stock in stocks_list:
-#     # Create empty list to store scraped tweets
-#         tweets_list = []
-#         # Search tweets since 2013 from the specified account whose text contains the keyword
-#         for i,tweet in enumerate(sntwitter.TwitterSearchScraper(stock + 'since:2013-01-01 until:2023-12-31 from:' + account).get_items()):
-#             if i>20000:
-#                 break
-#             # Disallow replies and retweets
-#             if "@" and "RT" not in tweet.content:
-#                 # Add relevant data to list
-#                 tweets_list.append([tweet.user.username, tweet.date, tweet.content])
-#             # Create dataframe from the tweets_list above
-#             tweets_df = pd.DataFrame(tweets_list, columns=['Username', 'Datetime', 'Text'])
-#             # Convert dataframe to CSV file with custom name
-#             tweets_df.to_csv(account + '_'+ stock +'_tweets.csv')        
