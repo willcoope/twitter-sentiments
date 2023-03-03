2f1c1f6a88399ce59b2c744abe029b2c8979e536
William Cooper <cooperwilliam92@gmail.com>
2023-03-03 12:49:58 +0000

    Add twitter_scrape method to main file

--- sentiment_analysis_test.py
+++ sentiment_analysis_test.py
@@ -10,6 +10,32 @@ import pandas as pd
 import datetime
 from dateutil.relativedelta import relativedelta
 from datetime import datetime
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+# Scrape Twitter for tweets from financial news accounts related to selected companies
+def twitter_scrape(): 
+    # Create list of accounts to search
+    accounts_list = ["CNBC","FT", "Reuters", "CNN", "TheEconomist", "BBCWorld","BBCNews", "BBCBreaking", "nytimes", "WSJ", "washingtonpost", "AP", "guardian", "SkyNews", "TelegraphNews", "ABC"]
+    stocks_list = ["Meta", "Netflix", "Tesla", "Amazon", "Pfizer", "Microsoft", "Disney", "Alphabet", "McDonald's", "Starbucks"]
+    #Iterate across accounts listed
+    for stock in stocks_list:
+        tweets_list = []
+        for account in accounts_list:
+        # Create empty list to store scraped tweets
+            # Search tweets since 2013 from the specified account whose text contains the keyword
+            for i,tweet in enumerate(sntwitter.TwitterSearchScraper(stock + ' since:2013-01-01 until:2023-01-01 from:' + account).get_items()):
+                if i>20000:
+                    break
+                # Disallow replies and retweets
+                if "@" and "RT" not in tweet.content:
+                    # Add relevant data to list
+                    tweets_list.append([tweet.user.username, tweet.date, tweet.content])
+        # Create dataframe from the tweets_list above
+        tweets_df = pd.DataFrame(tweets_list, columns=['Username', 'Datetime', 'Text'])
+        # Convert dataframe to CSV file with custom name
+        tweets_df.to_csv(stock +'_all_tweets.csv')
+
 
 # Remove hyperlinks, punctuation and special characters from tokens
 # Convert remaining tokens to a normal form (losing becomes lose, profits becomes profit)
